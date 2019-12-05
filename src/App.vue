2dbd5b1f089a8956b9402bf2e573cf728658b2b5
chenlong <1583479514@qq.com>
2019-12-05 14:28:18 +0800

    主题色设置

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,27 @@
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    document.documentElement.style.setProperty('--theme-color', '#f90')
+  }
+}
+</script>
+
 <style lang="scss">
 @import '@/assets/style/reset.scss';
 @import '@/assets/font/iconfont.scss';
+$theme-color: var(--theme-color);
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $theme-color;
 }
 #nav {
   padding: 30px;
