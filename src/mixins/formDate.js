fb468139c65da3e77301d1e464686b325bab9695
陈龙 <1583479514@qq.com>
2020-04-19 17:27:02 +0800

    lottery

--- src/mixins/formDate.js
+++
@@ -1,9 +0,0 @@
-import $ from '@/utils'
-export default {
-  methods: {
-    formDate (datetime) {
-      let formDate = $.formatDate(datetime, 'yyyy-MM-dd')
-      if (formDate === '9999-12-31') return true
-    }
-  }
-}
