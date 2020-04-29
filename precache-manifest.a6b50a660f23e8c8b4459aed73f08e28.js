4075e88a70ae7d209312822e2e567b760cfa58b6
chenlong <1583479514@qq.com>
2020-04-29 14:49:19 +0800

    update pages

---
+++ precache-manifest.a6b50a660f23e8c8b4459aed73f08e28.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b8dd42ec1bf0b4563c9",
+    "url": "static/js/about.e6d95745.js"
+  },
+  {
+    "revision": "5c4bd602537df53136b1",
+    "url": "static/css/app.f3d71bc9.css"
+  },
+  {
+    "revision": "5c4bd602537df53136b1",
+    "url": "static/js/app.9553c3eb.js"
+  },
+  {
+    "revision": "10acd1c8c20bf2be6b75",
+    "url": "static/js/chunk-vendors.3372a1a6.js"
+  },
+  {
+    "revision": "c0b59905178fbc1549ef",
+    "url": "static/css/countdown.d7d48b12.css"
+  },
+  {
+    "revision": "c0b59905178fbc1549ef",
+    "url": "static/js/countdown.7166b17b.js"
+  },
+  {
+    "revision": "8b1dbcef60ab06b536d2",
+    "url": "static/css/lottery.73c9f8b8.css"
+  },
+  {
+    "revision": "8b1dbcef60ab06b536d2",
+    "url": "static/js/lottery.15add4c4.js"
+  },
+  {
+    "revision": "100070d3f072ec5de1ec",
+    "url": "static/css/spike.51037271.css"
+  },
+  {
+    "revision": "100070d3f072ec5de1ec",
+    "url": "static/js/spike.7f5351e6.js"
+  },
+  {
+    "revision": "e9a689632109b88ad7bc",
+    "url": "static/css/spikeDetail.8ee9c6bf.css"
+  },
+  {
+    "revision": "e9a689632109b88ad7bc",
+    "url": "static/js/spikeDetail.580b3131.js"
+  },
+  {
+    "revision": "1ba5de8cdcd4bea5bb82",
+    "url": "static/js/spike~spikeDetail~uploader.90abd03a.js"
+  },
+  {
+    "revision": "49f1533f184d30a9fd9b",
+    "url": "static/css/timeLine.eed99f63.css"
+  },
+  {
+    "revision": "49f1533f184d30a9fd9b",
+    "url": "static/js/timeLine.7f93004d.js"
+  },
+  {
+    "revision": "ecc37cd6545a70176c9f",
+    "url": "static/css/uploader.1f48c733.css"
+  },
+  {
+    "revision": "ecc37cd6545a70176c9f",
+    "url": "static/js/uploader.a9a4b3cc.js"
+  },
+  {
+    "revision": "02404f13d27450f445c87635382d6e11",
+    "url": "0169ff7eca2a364b4edd.worker.js"
+  },
+  {
+    "revision": "6d1b7cc3b53127e82e2cb8bc38a5fd48",
+    "url": "static/img/icon-3.6d1b7cc3.png"
+  },
+  {
+    "revision": "d1f93e000c8c9042b0570e0d01fc824d",
+    "url": "static/img/success.d1f93e00.png"
+  },
+  {
+    "revision": "7ffdcb1e2d827fcfc458d882b2919b9c",
+    "url": "static/img/icon-4.7ffdcb1e.png"
+  },
+  {
+    "revision": "e5b46f4fecd4f360836ab8a868522076",
+    "url": "static/img/fail.e5b46f4f.png"
+  },
+  {
+    "revision": "a8e3725d6e563fbd73cb98949932e8be",
+    "url": "static/img/spike_banner.a8e3725d.png"
+  },
+  {
+    "revision": "5cf098a0e604a993fe2a848e9ba2b96c",
+    "url": "static/img/icon-2.5cf098a0.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "static/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b04e3d37e439584d61d22882865d61f7",
+    "url": "static/img/warning.b04e3d37.png"
+  },
+  {
+    "revision": "c1d3e390abd07035d2f11448e5d69a11",
+    "url": "static/img/bg1.c1d3e390.png"
+  },
+  {
+    "revision": "6f4ea53ec4cbb04d51d975a11b40b0bc",
+    "url": "static/img/money.6f4ea53e.png"
+  },
+  {
+    "revision": "9960dadef6b2035e202cb253aeb0c021",
+    "url": "static/img/bottom.9960dade.png"
+  },
+  {
+    "revision": "5a0037b62c3f047d788110e72cf05cd2",
+    "url": "static/img/title.5a0037b6.png"
+  },
+  {
+    "revision": "de5032d52563b6e1032f767deec646c6",
+    "url": "static/img/decoration.de5032d5.png"
+  },
+  {
+    "revision": "4e88c060860f8914206ae333ec58cc95",
+    "url": "static/img/base.4e88c060.png"
+  },
+  {
+    "revision": "0d4bf1f4b1cc307f09ad5891bcc1b8d3",
+    "url": "static/img/bg2.0d4bf1f4.png"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "static/img/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "9d273d014e91a73c2b3fed280881efda",
+    "url": "index.html"
+  },
+  {
+    "revision": "37555c81874f0a9f092b90ddaf81afa7",
+    "url": "light.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "1a72c26825463c2edba9b6a4af16a714",
+    "url": "test.html"
+  }
+];
